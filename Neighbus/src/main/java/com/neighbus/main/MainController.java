aab55c3ceb41b34d7fc6da39fd968f758bada53a
김정학 <wjdgkr3386@gmail.com>
2025-11-09 23:33:35 +0900

    main페이지 지역 선택을 DB에서 가져오기

--- Neighbus/src/main/java/com/neighbus/main/MainController.java
+++ Neighbus/src/main/java/com/neighbus/main/MainController.java
@@ -1,14 +1,33 @@
 package com.neighbus.main;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.neighbus.account.AccountDAO;
 
 @Controller
 public class MainController {
-
+	
+	@Autowired
+	AccountDAO accountDAO;
+	
 	@GetMapping(value="/")
-	public String mainForm() {
+	public String mainForm(
+		Model model
+	) {
 		System.out.println("MainController - mainForm");
+		
+	    //DB에서 대한민국 지역 가져오기
+		List<Map<String, Object>> provinceList = accountDAO.getProvince();
+		List<Map<String, Object>> regionList = accountDAO.getRegion();
+		model.addAttribute("provinceList", provinceList);
+		model.addAttribute("regionList", regionList);
+		
 		return "main/main";
 	}
 }
