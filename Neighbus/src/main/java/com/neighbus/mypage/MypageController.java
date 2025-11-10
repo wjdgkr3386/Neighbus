fa554902aef797612e59e4a683468b23d70fc288
mingyu <vkans7526@naver.com>
2025-11-10 10:44:12 +0900

    myPageMapper.xml 생성 " "

--- Neighbus/src/main/java/com/neighbus/mypage/MypageController.java
+++ Neighbus/src/main/java/com/neighbus/mypage/MypageController.java
@@ -2,6 +2,7 @@ package com.neighbus.mypage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,11 @@ public class MypageController {
 	public String mypageForm() {
 		System.out.println("MypageController - mypageForm");
 		return "mypage/mypage";
+	}
+	
+//	내정보 불러오기
+	@PostMapping("/upMypage")
+	public void upMypage() {
+		
 	}
 }
