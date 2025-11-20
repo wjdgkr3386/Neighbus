fa83390bc771ceb628962ab70055371c377b22be
library-min <library_mini@outlook.com>
2025-11-20 17:18:15 +0900

    관리자 페이지 완전 끝

--- Neighbus/src/main/java/com/neighbus/admin/AdminController.java
+++ Neighbus/src/main/java/com/neighbus/admin/AdminController.java
@@ -1,11 +1,17 @@
 package com.neighbus.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.neighbus.recruitment.RecruitmentDTO;
+import com.neighbus.recruitment.RecruitmentService;
 
 /**
  * ★ 관리자 전용 컨트롤러 ★
@@ -17,6 +23,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
+
+	@Autowired
+	private RecruitmentService recruitmentService;
 
 	/**
 	 * 관리자 대시보드 메인 페이지
@@ -65,6 +74,18 @@ public class AdminController {
 	 */
 	@GetMapping("/gatherings")
 	public String manageGatherings(Model model) {
+		// 모든 recruitment 데이터를 조회
+		List<RecruitmentDTO> recruitments = recruitmentService.findAllRecruitments();
+
+		// 각 recruitment의 참여인원 수를 Map에 저장
+		java.util.Map<Integer, Integer> memberCountMap = new java.util.HashMap<>();
+		for (RecruitmentDTO recruitment : recruitments) {
+			int memberCount = recruitmentService.countMembers(recruitment.getId());
+			memberCountMap.put(recruitment.getId(), memberCount);
+		}
+
+		model.addAttribute("recruitments", recruitments);
+		model.addAttribute("memberCountMap", memberCountMap);
 		return "admin/gatherings"; // admin/gatherings.html
 	}
 
