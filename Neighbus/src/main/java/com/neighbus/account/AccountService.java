1f9cde6d124f40378e77c469a5f94d13ce806ec3
mingyu <vkans7526@naver.com>
2025-11-21 15:29:59 +0900

    page 분리

--- Neighbus/src/main/java/com/neighbus/account/AccountService.java
+++ Neighbus/src/main/java/com/neighbus/account/AccountService.java
@@ -2,10 +2,16 @@ package com.neighbus.account;
 
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.List;
+import java.util.Map;
+
 public interface AccountService{
 
-	int insertSignup(AccountDTO accountDTO) throws Exception;
+	int insertSignup(AccountDTO accountDTO);
 	
 	UserDetails loadUserByUsername(String username) throws Exception;
-	
+
+	List<Map<String, Object>> getProvince();
+
+	List<Map<String, Object>> getCity();
 }
