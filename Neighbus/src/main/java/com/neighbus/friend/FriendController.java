70d4f8afb41c1958b02921c8dfbbd005f4566d45
김정학 <wjdgkr3386@gmail.com>
2025-11-25 15:08:06 +0900

    NEIG-30 친구 추가 기능 #진행 중

--- Neighbus/src/main/java/com/neighbus/friend/FriendController.java
+++ Neighbus/src/main/java/com/neighbus/friend/FriendController.java
@@ -1,5 +1,8 @@
 package com.neighbus.friend;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -15,20 +18,29 @@ public class FriendController {
 
 	@Autowired
 	FriendMapper friendMapper;
+	@Autowired
+	FriendService friendService;
 	
 	@GetMapping("/list")
 	public String friendList(
 		Model model,
 		@AuthenticationPrincipal AccountDTO user
 	) {
-		String myUUID = friendMapper.getMyUuid(user.getId());
-		int cnt = friendMapper.checkUuid("35bc20fb-682a-4cb3-84bc-a94a5b83921a");
+		String friendUuid = "35bc20fb-682a-4cb3-84bc-a94a5b83921a";
+		//친구 요청
+		int status = friendService.friendRequest(user, friendUuid);
+		model.addAttribute("status", status);
+//		//친구 수락
+//		int status2 = friendService.addFriend(user, friendId);
+//		model.addAttribute("status2", status2);
 		
-		System.out.println(myUUID);
-		System.out.println(cnt);
+//		int status3 = friendService.refuseFriend(user, friendId);
+//		model.addAttribute("status3", status3);
+		
+//		int status4 = friendService.deleteFriend(user, friendId);
+//		model.addAttribute("status4", status4);
 		
 		
-		model.addAttribute("myUUID", myUUID);
 		
 		return "friend/friendlist";
 	}
